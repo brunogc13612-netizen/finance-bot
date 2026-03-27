acf55741dd70bc020991aa51e8ce665d6e7ea7b2
brunogc13612-netizen <brunogc1361.2@gmail.com>
2026-03-27 14:14:49 -0300

    Creat a port

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,10 +2,25 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 from openai_client import interpretar_mensagem
 from sheets import salvar_no_sheets, ler_gastos
+from threading
+from https.server import HTTPServer, BaseHTTPRequestHandler
 import os
 
-TOKEN = os.getenv("TOKEN")
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot rodando!")
 
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+# roda servidor em paralelo
+threading.Thread(target=run_server).start()
+
+TOKEN = os.getenv("TOKEN")
 
 async def receber_mensagem(update: Update, context: ContextTypes.DEFAULT_TYPE):
     texto = update.message.text
