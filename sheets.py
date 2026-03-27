8fb66af5e792521027150aff567c3f91846c779b
brunogc13612-netizen <brunogc1361.2@gmail.com>
2026-03-27 11:25:42 -0300

    fix new sheets render

--- sheets.py
+++ sheets.py
@@ -3,20 +3,25 @@ from googleapiclient.discovery import build
 import os
 import json
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CREDENTIALS_PATH = os.path.join(BASE_DIR, "credentials.json")
+# 🔐 pega credenciais do Render
+credenciais_json = os.getenv("GOOGLE_CREDENTIALS")
 
-def salvar_no_sheets(dados):
-    
-    cred_json = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+if not credenciais_json:
+    raise Exception("GOOGLE_CREDENTIALS não configurado")
 
-    creds = service_account.Credentials.from_service_account_info(
-    cred_json,
+credenciais_dict = json.loads(credenciais_json)
+
+creds = service_account.Credentials.from_service_account_info(
+    credenciais_dict,
     scopes=["https://www.googleapis.com/auth/spreadsheets"]
 )
 
-    service = build("sheets", "v4", credentials=creds)
+service = build("sheets", "v4", credentials=creds)
 
+SHEET_ID = "1exQDyQurJtmhNlPb6sNlUIASSZG3fgGkr1icXL-T7e8"
+
+
+def salvar_no_sheets(dados):
     values = [[
         dados.get("data"),
         dados.get("pessoa"),
@@ -26,22 +31,16 @@ def salvar_no_sheets(dados):
     ]]
 
     service.spreadsheets().values().append(
-        spreadsheetId="1exQDyQurJtmhNlPb6sNlUIASSZG3fgGkr1icXL-T7e8",
+        spreadsheetId=SHEET_ID,
         range="A:E",
         valueInputOption="RAW",
         body={"values": values}
     ).execute()
 
+
 def ler_gastos():
-    creds = service_account.Credentials.from_service_account_file(
-        CREDENTIALS_PATH,
-        scopes=["https://www.googleapis.com/auth/spreadsheets"]
-    )
-
-    service = build("sheets", "v4", credentials=creds)
-
     result = service.spreadsheets().values().get(
-        spreadsheetId="1exQDyQurJtmhNlPb6sNlUIASSZG3fgGkr1icXL-T7e8",
+        spreadsheetId=SHEET_ID,
         range="A:E"
     ).execute()
 
